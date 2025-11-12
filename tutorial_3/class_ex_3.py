ad2c4b40b416baf9233f3d55e6f573a272d761f8
OrnitB <ornit321@gmail.com>
2025-11-12 10:04:52 +0200

    ex 1 done

--- tutorial_3/class_ex_3.py
+++ tutorial_3/class_ex_3.py
@@ -2,10 +2,11 @@
 import os
 import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
 
 os.getcwd()
 
-
+# ex 1
 penguins = sns.load_dataset("penguins")
 penguins_extra = pd.read_csv("penguins_extra.csv", index_col="Unnamed: 0")
 penguins.head()
@@ -18,3 +19,21 @@ penguins_extra.index = penguins_extra.index.astype(int)
 
 penguins_merge = pd.merge(penguins, penguins_extra, how="outer", left_index=True, right_index=True)
 print(penguins_merge)
+
+grouped_by_bill_length = penguins_merge.groupby(by=["island"])["bill_length_mm"].median()
+grouped_by_bill_length.head()
+
+plt.bar(penguins_merge["island"], height=penguins_merge["bill_length_mm"])
+
+plt.xlabel("Island")
+plt.ylabel("Bill Length (mm)")
+plt.title("Median Bill Length by Island")
+
+plt.show()
+
+penguins_merge["IQ"] = penguins_merge["IQ"].astype(float)
+group_by_iq_sex_preg = penguins_merge.groupby(by=["sex", "is_pregnant"])["IQ"].mean()
+
+print(group_by_iq_sex_preg)
+
+# ex 2
