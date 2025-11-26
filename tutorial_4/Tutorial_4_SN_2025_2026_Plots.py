24874ac88dce8ee0448d78604a958e08bce358ef
OrnitB <ornit321@gmail.com>
2025-11-26 18:39:41 +0200

    class ex 4 in progress

--- tutorial_4/Tutorial_4_SN_2025_2026_Plots.py
+++ tutorial_4/Tutorial_4_SN_2025_2026_Plots.py
@@ -55,27 +55,25 @@ font_lables = {'family': 'serif',
         'size': 13,
         }
 
-#Barplot
-
+#Barplot - Comparing numerical single values (mean, for example) among groups
 sns.barplot(data = penguins,x='island',y='bill_depth_mm',palette="Set2")
 plt.title('Penguins bill depth by island and sex',fontdict = font_title)
 plt.xlabel('Island',fontdict=font_lables)
 plt.ylabel('Bill depth [MM]',fontdict=font_lables)
 
-#Boxplot
-
+#Boxplot - Comparing THE SPREAD of each group with information of median, quarties and outliers
 sns.boxplot(data = penguins, x='island', y='flipper_length_mm',palette="Set2")
 plt.title('Penguins flipper length by island',fontdict = font_title)
 plt.xlabel('Island',fontdict=font_lables)
 plt.ylabel('Bill depth [MM]',fontdict=font_lables)
 
-#Violinplot
+#Violinplot - Like boxplot, but with density as well
 sns.violinplot(data=mpg,x="origin",y='mpg',palette="Set2",fill=False,width=.5)
 plt.title('Car fuel consumption by country of origin',fontdict = font_title)
 plt.xlabel('Country of Origin',fontdict=font_lables)
 plt.ylabel('MPG',fontdict=font_lables)
 
-#Scatterplot
+#Scatterplot - Using dots to represent values for two different numeric variables
 sns.relplot(data=iris, x='sepal_length', y= 'petal_length', size="petal_width",
             kind='scatter',palette="Set2",hue='species')
 plt.title('Petal length as function of sepal length',fontdict = font_title)
@@ -83,7 +81,7 @@ plt.xlabel('Sepal Length [mm]',fontdict=font_lables)
 plt.ylabel('Petal Length [mm]',fontdict=font_lables)
 # size = 'petal_width'
 
-#Histogram
+#Histogram - Plots the distribution of a numeric variable's values as a series of bars
 sns.histplot(data=penguins,x='flipper_length_mm',hue="island")
 plt.title('Flipper Length Distribution Amongst Penguins',fontdict = font_title)
 plt.xlabel('Flipper Length [mm]',fontdict=font_lables)
@@ -91,7 +89,7 @@ plt.ylabel('Count',fontdict=font_lables)
 plt.show()
 sns.color_palette()
 
-#Density plot
+#Density plot - Like the histogram, but continuous
 sns.kdeplot(data=diamonds,x='price',hue='cut')
 plt.title('Diamonds Prices Distribution',fontdict = font_title)
 plt.xlabel('Diamond price [$]',fontdict=font_lables)
@@ -99,7 +97,7 @@ plt.ylabel('Count',fontdict=font_lables)
 #Line chart
 sns.relplot(data=diamonds,x="carat",y="price",kind="line")
 
-#Linear regression
+#Linear regression - Correlation between two numerical variables
 sns.regplot(
     data=mpg, x="weight", y="horsepower",
     ci=99, marker="x", color=".3", line_kws=dict(color="r")
@@ -110,7 +108,7 @@ plt.title(f"Weight as function of Horsepower. r={corr:.2f}, p={p_value:.2e}",fon
 plt.xlabel('Weight [KG]',fontdict=font_lables)
 plt.ylabel('Horsepower',fontdict=font_lables)
 
-#Line Chart
+#Line Chart - To show numerical variables as function of continuous numerical variables (mainly time)
 x = [1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14]
 y = [10, 30, 70, 90, 100, 105, 110, 113, 117, 120, 121, 122, 122, 122]
 
@@ -120,7 +118,7 @@ plt.xlabel('Time [min]',fontdict=font_lables)
 plt.ylabel('OD (600nm)',fontdict=font_lables)
 
 
-#Pie charts
+#Pie charts - Distribution of categorical values among data
 values= [25,15,45,15]
 labels= ["Arabs","Ultra-orthodox","Secular","Religious"]
 plt.pie(values,labels=labels)
